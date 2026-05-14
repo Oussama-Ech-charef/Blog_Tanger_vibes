a7fe8c7d6818b38773eccfc9b8bf582bea356f6f
OUSSAMA <oussamacharef71@gmail.com>
2026-05-14 17:51:07 +0200

    Add header, main styles, homepage image, header include, and homepage

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<?php 
+ 
+
+
+?>
+
+
+<header class="site_header">
+    <!-- logo  -->
+        <nav class="header_nav">
+                    <a href="#" class="logo">
+                        <div class="logo_icon"><i class="fa-solid fa-compass"></i></div> 
+                        <span class="logo_text">Tangier <span class="text2">Vibes</span></span>     
+                    </a>
+
+                     <!-- links  desctope-->
+
+                    <ul class="nav_links desktop">
+                        <li><a href="#" class="nav_link">Home</a></li>
+                        <li><a href="#" class="nav_link">Explore</a></li>
+                    </ul>
+
+                   <!-- search desctop -->
+                    <div class="search_desctop">
+                        <form action="#" class="search_desctop_form">
+                            <i class="fa-solid fa-magnifying-glass search_icon"></i>
+                            <input type="text" placeholder="Search places...">
+                        </form>
+
+                    </div>
+                    <div class="auth_actions_desctop">
+
+                            <!-- <div class="dashboard_logout">
+                                <a href="#" class="dashboard">Dashboard</a>
+                                <a href="#" class="logout">Logout</a>
+                            </div> -->
+
+
+                            <div class="login_register">
+                                <a href="#" class="login_link">Login</a>
+                                <a href="#" class="login_link">Register</a>
+                            </div>
+                    </div>
+
+                    <!-- <div class="menu">
+                        <button class="menu_btn" id="menu_btn">
+                            <i class="fa-solid fa-bars"></i>
+                        </button>
+                    </div> -->
+
+
+        </nav>
+
+        
+
+
+
+</header>
