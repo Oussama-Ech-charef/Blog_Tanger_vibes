4de29c33fb7e3b9e14f2d867f2bb0d94ef69189b
OUSSAMA <oussamacharef71@gmail.com>
2026-05-15 23:58:19 +0100

    Add responsive header markup

--- includes/header.php
+++ includes/header.php
@@ -4,10 +4,11 @@
 
 ?>
 
-
 <header class="site_header">
-    <!-- logo  -->
-        <nav class="header_nav">
+
+
+        <div class="header_nav_desctop">
+                    <!-- logo  -->
                     <a href="#" class="logo">
                         <div class="logo_icon"><i class="fa-solid fa-compass"></i></div> 
                         <span class="logo_text">Tangier <span class="text2">Vibes</span></span>     
@@ -28,28 +29,74 @@
                         </form>
 
                     </div>
+
+                    
                     <div class="auth_actions_desctop">
 
-                            <!-- <div class="dashboard_logout">
+                            <div class="dashboard_logout">
                                 <a href="#" class="dashboard">Dashboard</a>
                                 <a href="#" class="logout">Logout</a>
-                            </div> -->
+                            </div>
 
 
                             <div class="login_register">
                                 <a href="#" class="login_link">Login</a>
-                                <a href="#" class="login_link">Register</a>
+                                <a href="#" class="register_link ">Register</a>
                             </div>
                     </div>
-
-                    <!-- <div class="menu">
+                        <!-- menu open  -->
+                    <div class="menu">
                         <button class="menu_btn" id="menu_btn">
                             <i class="fa-solid fa-bars"></i>
                         </button>
-                    </div> -->
+                    </div>
+                        
 
 
-        </nav>
+        </div>
+        <div class="header_nav_mobile">
+
+                    <!-- close menu  -->
+                    <div class="mobile_menu_header">
+                        <button class="close_menu" id="close_menu" aria-label="Fermer le menu">
+                                    <i class="fa-solid fa-xmark"></i>
+                        </button>
+                    </div>
+
+                     <!-- links  desctope-->
+
+                    <ul class="nav_links mobil">
+                        <li><a href="#" class="nav_link">Home</a></li>
+                        <li><a href="#" class="nav_link">Explore</a></li>
+                    </ul>
+
+                   <!-- search desctop -->
+                    <div class="search_mobile">
+                        <form action="#" class="search_mobile_form">
+                            <i class="fa-solid fa-magnifying-glass search_icon"></i>
+                            <input type="text" placeholder="Search places...">
+                        </form>
+
+                    </div>
+
+
+
+                    <div class="auth_actions_mobil">
+
+                            <div class="dashboard_logout">
+                                <a href="#" class="dashboard">Dashboard</a>
+                                <a href="#" class="logout">Logout</a>
+                            </div>
+
+
+                            <div class="login_register">
+                                <a href="#" class="login_link">Login</a>
+                                <a href="#" class="register_link ">Register</a>
+                            </div>
+                    </div>
+
+
+        </div>
 
         
 
