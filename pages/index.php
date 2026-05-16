2e58b6221cfe1c78b746a18ddc9fa75cabc6127c
OUSSAMA <oussamacharef71@gmail.com>
2026-05-16 01:18:33 +0100

    Add home hero section

--- pages/index.php
+++ pages/index.php
@@ -10,7 +10,7 @@
     
     <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="../assets/css/header.css">
-
+    <link rel="stylesheet" href="../assets/css/home.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
 </head>
 <body>
@@ -18,7 +18,27 @@
 
 <?php require '../includes/header.php'; ?>
 
-<img src="../assets/images/home.jpg" alt="" style="width: 200px;">
+        <section class="hero_section">
+            <img src="../assets/images/home.jpg"  alt="Tanger Vibes" loading="lazy">
+            <div class="hero_shadow"></div>
+
+
+            <div class="hero_content">
+               
+                <p class="hero_label">WELCOME TO YOUR GATEWAY TO AFRICA</p>
+                <h1 class="hero_title">Experience the Magic<br> of <span class="hero_highlight">Tangier</span></h1>
+
+                <p class="hero_desc">Discover hidden beaches, legendary cafes, exquisite<br>restaurants, and historic landmarks in the Pearl of the North.</p>
+                
+                <div class="hero_btns">
+
+                    <a href="explore.php" class="btn_explor">
+                        Start Exploring
+                    </a>
+
+                </div>
+            </div>
+        </section>
 
 
 <?php require '../includes/footer.php' ?>
