e83718cca0ac53bf6514cbbd48ca86550bb97785
OUSSAMA <oussamacharef71@gmail.com>
2026-05-17 01:54:31 +0100

    Add post detail page

---
+++ pages/detail.php
@@ -0,0 +1,143 @@
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tangier Vibes</title>
+    
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    
+   
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/detail.css">
+    
+</head>
+<body>
+
+
+<?php require '../includes/header.php' ?>
+
+    <div class="detail_container">
+
+        <div class="category">
+            <i class="fa-solid fa-layer-group"></i> TANGERE / <span class="cat_name">BEACH</span>
+        </div>
+
+
+        <h1>Lorem ipsum dolor sit amet.</h1>
+
+        <div class="icons">
+            <span><i class="fa-solid fa-calendar-days"></i>May 17, 2026</span>
+            <span><i class="fa-solid fa-circle-user"></i>By Oussama</span>
+            <span><i class="fa-solid fa-comment-dots"></i></i>8 Comments</span>
+        </div>
+
+        <img src="../assets/images/home.jpg" width="400px" alt="detial image">
+
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Quia voluptates, id accusantium cons
+                ectetur a possimus, debitis doloribus ipsa illo ea eos 
+                et sequi commodi! Nisi quis magnam expedita nemo omnis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolores repudiandae culpa fugiat qui ipsa consequatur tempora harum pariatur nulla dolore totam doloribus, dolorum voluptates numquam eum accusantium voluptatibus tenetur. Error cum vero adipisci inventore praesentium illo, laborum, at optio ipsam cupiditate libero quod, perferendis quia neque voluptate corrupti a eius quam soluta iste! Veniam dolorum earum voluptatum eveniet iusto corrupti, non sit eum repellendus. Maiores, quis! Odit, similique! Earum iste ab repellat. Ratione ipsum soluta ullam amet ad eos, tenetur molestiae enim voluptas eius voluptatem aspernatur voluptatum. Eum repellat modi magni aperiam dicta cumque nihil eligendi pariatur, cupiditate assumenda exercitationem nemo quasi reprehenderit, iure recusandae neque quidem veniam totam aliquid nisi! Magni distinctio amet, autem ipsa numquam porro voluptate veritatis nostrum assumenda veniam tempore magnam ullam saepe accusamus velit ipsam. Veniam dolorem magnam velit aperiam iste illum aut optio quod dolores asperiores, molestias consequuntur vel cum error dignissimos voluptas at omnis a fugiat ullam tenetur nobis obcaecati delectus. Rem id maxime quod nemo doloribus quo numquam reiciendis nostrum mollitia. Nostrum incidunt esse ipsum laudantium nihil sit deserunt ullam ut perferendis voluptatibus, doloribus vel sint excepturi iure, officiis voluptate minima, blanditiis tenetur? Consequuntur id obcaecati assumenda tempora totam, minus saepe, incidunt aliquid nulla fuga sit fugiat eius accusantium? Corrupti eum odio possimus, earum nostrum numquam. Sequi soluta, consectetur ut eaque sapiente eligendi officiis pariatur a quis quas? Corporis voluptas nihil et dignissimos unde placeat, omnis id officia repellendus! Ut amet aspernatur, quo minus praesentium, officiis itaque iure maiores neque ducimus eaque! Nulla officia, fugiat distinctio nihil odio porro aut veniam quidem, quibusdam blanditiis modi, officiis ducimus laboriosam doloremque tempore iusto ut ab! Rem exercitationem iure atque ipsum adipisci nostrum minus beatae asperiores corrupti? Accusamus eius natus sit beatae, aperiam quia impedit deserunt libero tenetur quisquam ullam totam suscipit obcaecati minus ex reiciendis itaque assumenda accusantium iusto quod veritatis. Nisi repudiandae eveniet suscipit deleniti nesciunt rerum placeat autem dolor consectetur ratione rem optio, error hic molestias amet cupiditate quisquam ut, perspiciatis minus, molestiae voluptatibus numquam magni cumque delectus. Quaerat voluptate ducimus culpa omnis rerum expedita impedit ea nesciunt inventore itaque, atque aliquid vel odio animi vero fugiat velit !</p>
+        </div>
+
+
+        <div class="tags">
+            <span><i class="fa-solid fa-hashtag"></i>Tanegere</span>
+            <span><i class="fa-solid fa-hashtag"></i>SlowTravel</span>
+            <span><i class="fa-solid fa-hashtag"></i>DesertVibes</span>
+            <span><i class="fa-solid fa-hashtag"></i>NomadLife</span>
+        </div>
+
+
+        <div class="social">
+            <i class="fas fa-share-alt"></i> Share: <a href="#">Facebook</a> /<a href="#">Twitter</a> /<a href="#">WhatsApp</a>
+        </div>
+
+        <div class="location">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.0!2d-5.8137!3d35.7595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0b875cf1b23c99%3A0x4c6d8cc17ce7b75b!2sTanger%2C%20Morocco!5e0!3m2!1sen!2sma!4v1700000000000"
+                width="100%"
+                height="400"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
+
+
+        <div class="comments_posts">
+            <div class="comment_title">
+                💬 Comments 
+                <span >3</span>
+            </div>
+        </div>
+
+        <div class="coments_list">
+            <div class="coment_item">
+                <div class="comment_header">
+                    <span class="coment_name"><i class="fa-solid fa-circle-user"></i>Oussama</span>
+                    <span class="coment_date"><i class="fa-solid fa-calendar-days"></i>May 16, 2026</span>
+                </div>
+                <div class="comment_text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, saepe.
+                </div>
+            </div>
+            <div class="coment_item">
+                <div class="comment_header">
+                    <span class="coment_name"><i class="fa-solid fa-circle-user"></i>Oussama</span>
+                    <span class="coment_date"><i class="fa-solid fa-calendar-days"></i>May 16, 2026</span>
+                </div>
+                <div class="comment_text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, saepe.
+                </div>
+            </div>
+            <div class="coment_item">
+                <div class="comment_header">
+                    <span class="coment_name"><i class="fa-solid fa-circle-user"></i>Oussama</span>
+                    <span class="coment_date"><i class="fa-solid fa-calendar-days"></i>May 16, 2026</span>
+                </div>
+                <div class="comment_text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, saepe.
+                </div>
+            </div>
+
+        </div>
+
+        <div class="comment_form">
+            <h3 class="coment_title">Leave a comment</h3>
+
+            <form action="#" method="POST">
+                <div class="form_name">
+                    <label>Your name :</label>
+                    <input type="text" name="name" placeholder="e.g., Ahmed or Fatima">
+                </div>
+                <div class="form_desc">
+                    <label>Your message :</label>
+                    <textarea name="message" placeholder="Share your vibe..."></textarea>
+                </div>
+                <button type="submit"><i class="fa-solid fa-paper-plane"></i> Post comment</button>
+            </form>
+        </div>
+
+
+    </div>
+
+
+
+
+    <script src="../assets/js/main.js"></script>
+</body>
+
+</html>
