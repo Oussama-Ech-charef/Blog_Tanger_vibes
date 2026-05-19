16556ed8b4af2b2cf1b52edb128e5bc54742deb5
OUSSAMA <oussamacharef71@gmail.com>
2026-05-19 02:48:47 +0100

    Start sessions on public pages

--- pages/explor.php
+++ pages/explor.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require '../config/connection.php';
 
 $stmt = $conn->prepare("
--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,5 @@
-
 <?php
-
+session_start();
  require '../config/connection.php';
 
  $stmt = $conn->prepare("
